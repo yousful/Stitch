bd3c2946b5f11c87ebea5638eb67309bae530f71
susanodii <odiisusan@gmail.com>
2024-08-29 14:14:23 +0100

    build a difference

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "./Hero";
 import Review from "./Review";
 import TopRated from "./TopRated";
+import OurDiffernce from "./OurDiffernce";
 function Home() {
   return (
     <main className='min-h-screen h-full pl-[2rem]  pr-[2rem]'>
       <Hero />
       <Review />
       <TopRated />
+      <OurDiffernce />
     </main>
   );
 }
