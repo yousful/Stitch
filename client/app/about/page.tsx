ff065026e61e143290cb519a433f5a4cf1e5bfe0
susanodii <odiisusan@gmail.com>
2024-09-01 02:19:13 +0100

    our service

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Explore from "./Explore";
+import OurService from "./OurService";
 
 export default function page() {
   return (
     <div className='min-h-screen h-full pl-[4.5rem] pr-[4.5rem] bg-accent pt-4'>
       <Explore />
+      <OurService />
     </div>
   );
 }
