430cba8816de1467acfc481863cc32ef0117408f
susanodii <odiisusan@gmail.com>
2024-09-02 10:31:55 +0100

    hero section find a tailor

--- client/app/find/page.tsx
+++ client/app/find/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Hero";
 
 const page = () => {
-  return <div>findgmee</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default page;
