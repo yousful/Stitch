d765af7614a28c2653f1665b4210b3b33932f910
susanodii <odiisusan@gmail.com>
2024-09-02 11:09:10 +0100

    Find a talor page

--- client/app/find/page.tsx
+++ client/app/find/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import FindATailorLayout from "./FindATailorLayout";
 
 const page = () => {
   return (
     <div>
       <Hero />
+      <FindATailorLayout />
     </div>
   );
 };
